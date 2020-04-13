ffcb8cbdfe288305dfce6ef70afd87b405e3dcb4
carson <carsonPan@outlook.com>
2020-04-13 20:10:06 +0800

    优化

--- src/Core/ResourceIdProvider.cs
+++ src/Core/ResourceIdProvider.cs
@@ -23,7 +23,7 @@ namespace AuthorizationExtension.Core
 
         protected ResourceOptions GetResourceOptions()
         {
-            return AuthorizationExtensionOptionsAccessor.CurrentValue?.Resource??new ResourceOptions();
+            return AuthorizationExtensionOptionsAccessor.CurrentValue?.Resource ?? new ResourceOptions();
         }
 
         protected HttpRequest GetHttpRequest()
@@ -35,7 +35,39 @@ namespace AuthorizationExtension.Core
             }
             return request;
         }
-        protected string GetRoutePatternText(RouteEndpoint routeEndpoint)
+        //protected string GetRoutePatternText(RouteTemplate routeTemplate)
+        //{
+        //    StringBuilder templateBuilder = new StringBuilder();
+        //    foreach (TemplateSegment segment in routeTemplate.Segments)
+        //    {
+        //        templateBuilder.Append("/");
+        //        foreach (TemplatePart part in segment.Parts)
+        //        {
+        //            if (part.IsParameter)
+        //            {
+        //                templateBuilder.Append("{").Append(part.Name).Append("}");
+        //            }
+        //            else
+        //            {
+        //                templateBuilder.Append(part.Text);
+        //            }
+        //        }
+        //    }
+        //    return templateBuilder.ToString();
+        //}
+
+
+        protected IEnumerable<string> GetRequiredKeys()
+        {
+            ResourceOptions resourceOptions = GetResourceOptions();
+            if (resourceOptions.CustomRouteKeys.IsNullOrEmpty())
+            {
+                return resourceOptions.RequiredRouteKeys;
+            }
+            return resourceOptions.RequiredRouteKeys.Union(resourceOptions.CustomRouteKeys);
+        }
+
+        public string GetResourceId(RouteEndpoint routeEndpoint)
         {
             StringBuilder templateBuilder = new StringBuilder();
             IEnumerable<RoutePatternPathSegment> pathSegments = routeEndpoint.RoutePattern.PathSegments;
@@ -43,6 +75,7 @@ namespace AuthorizationExtension.Core
             {
                 return null;
             }
+            IDictionary<string, object> values = GetRequiredValues();
             foreach (RoutePatternPathSegment segment in pathSegments)
             {
                 templateBuilder.Append("/");
@@ -61,84 +94,89 @@ namespace AuthorizationExtension.Core
                     else
                     {
                         RoutePatternParameterPart parameterPart = part as RoutePatternParameterPart;
-                        templateBuilder.Append("{").Append(parameterPart.Name).Append("}");
+                        templateBuilder.Append("{");
+                        if (parameterPart.IsCatchAll)
+                        {
+                            templateBuilder.Append("*");
+                            if (!parameterPart.EncodeSlashes)
+                            {
+                                templateBuilder.Append("*");
+                            }
+                        }
+                        if (values.ContainsKey(parameterPart.Name))
+                        {
+                            templateBuilder.Append(values[parameterPart.Name]);
+                        }
+                        else
+                        {
+                            templateBuilder.Append(parameterPart.Name);
+                        }
+                        if (parameterPart.IsOptional)
+                        {
+                            templateBuilder.Append("?");
+                        }
+                        //需不要考虑规则呢？ 例如5:int 
+                        //if (!parameterPart.ParameterPolicies.IsNullOrEmpty())
+                        //{
+
+                        //}
+                        templateBuilder.Append("}");
                     }
                 }
             }
             return templateBuilder.ToString();
         }
 
-        protected string GetRoutePatternText(RouteTemplate routeTemplate)
+        private IDictionary<string, object> GetRequiredValues()
         {
-            StringBuilder templateBuilder = new StringBuilder();
-            foreach (TemplateSegment segment in routeTemplate.Segments)
+            IEnumerable<string> requiredKeys = GetRequiredKeys();
+            Dictionary<string, object> values = new Dictionary<string, object>();
+            if (requiredKeys.IsNullOrEmpty())
             {
-                templateBuilder.Append("/");
-                foreach (TemplatePart part in segment.Parts)
-                {
-                    if (part.IsParameter)
-                    {
-                        templateBuilder.Append("{").Append(part.Name).Append("}");
-                    }
-                    else
-                    {
-                        templateBuilder.Append(part.Text);
-                    }
-                }
-            }
-            return templateBuilder.ToString();
-        }
-        protected virtual string GetRoutePatternText()
-        {
-            Endpoint endpoint = HttpContextAccessor?.HttpContext.GetEndpoint();
-            if (endpoint is RouteEndpoint routeEndpoint)
-            {
-                return GetRoutePatternText(routeEndpoint);
-            }
-            RouteData routeData = HttpContextAccessor?.HttpContext.GetRouteData();
-            Route router = routeData.Routers.GetOrDefault(1) as Route;
-            if (router != null)
-            {
-                return GetRoutePatternText(router.ParsedTemplate);
-            }
-            return null;
-        }
-
-        protected IEnumerable<string> GetRequiredKeys()
-        {
-            ResourceOptions resourceOptions = GetResourceOptions();
-            if(resourceOptions.CustomRouteKeys.IsNullOrEmpty())
-            {
-                return resourceOptions.RequiredRouteKeys;
-            }
-            return resourceOptions.RequiredRouteKeys.Union(resourceOptions.CustomRouteKeys);
-        }
-        public string GetResourceId()
-        {
-            HttpRequest request = GetHttpRequest();
-            string routePatternText = GetRoutePatternText();
-            if (string.IsNullOrWhiteSpace(routePatternText))
-            {
-                return $"{request.Method}|{request.Scheme}|{request.PathBase}{request.Path}";
+                return values;
             }
             RouteData routeData = HttpContextAccessor?.HttpContext.GetRouteData();
             if (routeData == null || routeData.Values.Count == 0)
             {
-                return $"{request.Method}|{request.Scheme}|{routePatternText}";
-            }
-            IEnumerable<string> requiredKeys=GetRequiredKeys();
-            if(requiredKeys.IsNullOrEmpty())
-            {
-                return $"{request.Method}|{request.Scheme}|{routePatternText}";
+                return values;
             }
             foreach (string key in routeData.Values.Keys)
             {
-                    if (requiredKeys.Any(i => i == key))
-                    {
-                        routePatternText =routePatternText.Replace("{" + key + "}", routeData.Values[key].ToString());
-                    }
+                if (requiredKeys.Any(i => i == key))
+                {
+                    values.Add(key, routeData.Values[key]);
+                }
             }
-            return $"{request.Method}|{request.Scheme}|{routePatternText}";
+            return values;
+        }
+        public string GetResourceId()
+        {
+            HttpRequest request = GetHttpRequest();
+            Endpoint endpoint = HttpContextAccessor?.HttpContext.GetEndpoint();
+            string resourceId = null;
+            if (endpoint is RouteEndpoint routeEndpoint)
+            {
+                resourceId = GetResourceId(routeEndpoint);
+            }
+            else
+            {
+                throw new NotSupportedException("获取RouteEndpoint失败！");
+            }
+            //放弃对传统路由的支持
+            //else
+            //{
+            //    RouteData routeData = HttpContextAccessor?.HttpContext.GetRouteData();
+            //    Route router = routeData.Routers.GetOrDefault(1) as Route;
+            //    if (router != null)
+            //    {
+            //        resourceId= GetRoutePatternText(router.ParsedTemplate);
+            //    }
+            //}
+            if (string.IsNullOrWhiteSpace(resourceId))
+            {
+                return $"{request.Method}|{request.Scheme}|{request.PathBase}{request.Path}";
+            }
+            return $"{request.Method}|{request.Scheme}|{resourceId}";
         }
     }
 }
